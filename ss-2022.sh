29ccf26c380ed974e5464b86c586b68c2a6c915d
DWI589 <154402276+DWI589@users.noreply.github.com>
2026-04-24 00:34:40 +0800

    Rename '错误' to 'Error' for consistency

--- ss-2022.sh
+++ ss-2022.sh
@@ -162,7 +162,7 @@ get_latest_version() {
 
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m" && Yellow_font_prefix="\033[0;33m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-错误="${Red_font_prefix}[错误]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Yellow_font_prefix}[注意]${Font_color_suffix}"
 
 check_installed_status() {
